408a661a4b53dd6b16642f4ba47747ba07bd01e7
jiagangzhang <jiagangzhang@Mac-WWE-Jiagang.local>
2016-10-11 17:27:54 +0800

    commit

---
+++ performanceTest/scp-mobile.sh
@@ -0,0 +1,5 @@
+#just mobile
+scp -i ~/keys/QA_Team.pem /Users/enochzheng/Workspaces/mm-qa-performance-tests/scripts/mobile-scenario.jmx mmadmin@122.112.213.135:/home/mmadmin/enochzheng/mm-qa-performance-tests/scripts
+# test segments
+#scp -i ~/keys/QA_Team.pem -r /Users/enochzheng/Workspaces/mm-qa-performance-tests/scripts/mobile-scenarios mmadmin@122.112.213.135:/home/mmadmin/enochzheng/mm-qa-performance-tests/scripts 
+#scp -i ~/keys/QA_Team.pem -r /Users/enochzheng/Workspaces/mm-qa-performance-tests/scripts/ mmadmin@122.112.213.135:/home/mmadmin/enochzheng/mm-qa-performance-tests/
---
+++ readme.txt
@@ -0,0 +1,1 @@
+Order_CreateOrder is committed to MM-18573 uat-lw branch. Need to merge this file to master and other mobile scenarios
